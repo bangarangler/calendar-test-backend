943ca95ca0f1ed56fe146ba8803de1e2012bd274
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-26 09:35:19 -0400

    Small tweak converted event start and end...
    to strings so that i could then get the correct time with .toISOString

--- models/event.js
+++ models/event.js
@@ -12,11 +12,11 @@ const eventSchema = new Schema({
     required: true,
   },
   start: {
-    type: Date,
+    type: String,
     required: true,
   },
   end: {
-    type: Date,
+    type: String,
     required: true,
   },
 });
