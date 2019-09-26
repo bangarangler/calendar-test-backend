209b8a3324a5e6601aaecfb20af6230a9fe507b6
Jonathan Dain Palacio <bangarangler@gmail.com>
2019-09-26 11:33:46 -0400

    Added Header info to allow front end to hit test db

--- index.js
+++ index.js
@@ -14,6 +14,19 @@ app.use(cors());
 app.use(helmet());
 app.use(compression());
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader(`Access-Control-Allow-Origin`, `*`);
+  res.setHeader(
+    `Access-Control-Allow-Methods`,
+    `OPTIONS, GET, POST, PUT, PATCH, DELETE`,
+  );
+  res.setHeader(`Access-Control-Allow-Headers`, `Content-Type, Authorization`);
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(eventRoutes);
 
 
